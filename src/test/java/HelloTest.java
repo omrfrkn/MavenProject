9c37ddc3877c7ce180b28e4e6e532970917ebf0c
omrfrkn <ofurkandogan@gmail.com>
2022-02-22 12:27:16 +0300

    added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,7 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         System.out.println("hello future");
-
-
+        System.out.println("Local Repo is already published");
+        System.out.println("Doing more work");
     }
 }
